2bbc6419e6e70e7391599ffa81d3952dd04ea734
MokkeMeguru <meguru.mokke@gmail.com>
2021-04-18 16:11:18 +0900

    update bbsk install script

--- install.sh
+++ install.sh
@@ -8,4 +8,8 @@ then
     mkdir $HOME/.bbsk
 fi
 
+if [ $(grep "alias bb=" $HOME/.bashrc | wc -l) == 0 ]
+then
 bash <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install) $HOME/.bbsk/
+echo "alias bb=$HOME/.bbsk/bb" >> $HOME/.bashrc
+fi
